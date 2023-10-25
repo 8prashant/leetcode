f1da81d9315ddaed1950f5acfc9cbccbf4a17c7c
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2023-10-25 13:56:45 +0530

    Time: 57 ms (84.57%), Space: 30.7 MB (74.46%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,52 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        int n = mat[0].size();
-        vector<vector<int>> dp;
-        
-        for (vector<int>& row: mat) {
-            dp.emplace_back(row.begin(), row.end());
-        }
-        
-        for (int row = 0; row < m; row++) {
-            for (int col = 0; col < n; col++) {
-                if (dp[row][col] == 0) {
-                    continue;
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>> v(mat.size(),vector<int>(mat[0].size(),-1));
+        queue<pair<int,int>>q;
+        for(int i=0;i<mat.size();i++){
+            for(int j=0;j<mat[0].size();j++){
+                if(mat[i][j]==0){
+                    v[i][j]=0;
+                    q.push({i,j});
                 }
-
-                int minNeighbor = m * n;
-                if (row > 0) {
-                    minNeighbor = min(minNeighbor, dp[row - 1][col]);
-                }
-                
-                if (col > 0) {
-                    minNeighbor = min(minNeighbor, dp[row][col - 1]);
-                }
-                
-                dp[row][col] = minNeighbor + 1;
             }
         }
-        
-        for (int row = m - 1; row >= 0; row--) {
-            for (int col = n - 1; col >= 0; col--) {
-                if (dp[row][col] == 0) {
-                    continue;
+        vector<int> dx = {-1, 0, 1, 0};
+        vector<int> dy = {0, -1, 0, 1};
+        while(!q.empty()){
+            int x=q.front().first;
+            int y=q.front().second;
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int xn=x+dx[i];
+                int yn=y+dy[i];
+                if(xn>=0 && xn<m && yn>=0 && yn<n && v[xn][yn]==-1){
+                    v[xn][yn]=v[x][y]+1;
+                    q.push({xn,yn});
                 }
-                
-                int minNeighbor = m * n;
-                if (row < m - 1) {
-                    minNeighbor = min(minNeighbor, dp[row + 1][col]);
-                }
-                
-                if (col < n - 1) {
-                    minNeighbor = min(minNeighbor, dp[row][col + 1]);
-                }
-                
-                dp[row][col] = min(dp[row][col], minNeighbor + 1);
             }
         }
-        
-        return dp;
+        return v;
     }
 };
