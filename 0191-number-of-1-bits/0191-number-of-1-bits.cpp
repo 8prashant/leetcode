7f5d28e4dd6091e50411d6c72f9959923119cdef
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2023-11-29 22:57:21 +0530

    Time: 4 ms (30.61%), Space: 6.4 MB (57.22%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -4,10 +4,12 @@ public:
       //return n.count(1);  
       int count=0;
       while(n!=0){
-        if((n>>1)<<1!=n){
+          n=n&(n-1);//Brian kerningam's algorithm
+          count++;
+        /*if((n>>1)<<1!=n){
           count++;
         }
-        n>>=1;
+        n>>=1;*/
       }
       return count;
     }
