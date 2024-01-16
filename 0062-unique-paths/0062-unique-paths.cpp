b70d1e45f75628ea69d6bd5a4fdcdad4cb5c5495
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-01-16 17:10:29 +0530

    Time: 0 ms (100%), Space: 6.9 MB (40.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,11 +1,21 @@
-#include <cmath>
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        long long ans = 1;
-        for (int i = 1; i <= m - 1; ++i) {
-            ans = ans * (n - 1 + i) / i;
+        vector<vector<int>> ans(m,vector<int>(n,0));
+        ans[0][0]=1;
+        vector<int> dx={1,0};
+        vector<int> dy={0,1};
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<2;k++){
+                    int nx=i+dx[k];
+                    int ny=j+dy[k];
+                    if(nx<m && ny<n){
+                        ans[nx][ny]+=ans[i][j];
+                    }
+                }
+            }
         }
-        return (int)ans;
+        return ans[m-1][n-1];
     }
 };
