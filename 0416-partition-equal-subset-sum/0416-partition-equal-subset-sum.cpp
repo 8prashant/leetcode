4df2358a041d8f21d4fbf332d11667cfebd1fdb4
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-01-28 10:24:49 +0530

    Time: 340 ms (44.36%), Space: 16 MB (62.28%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,25 +1,25 @@
-// memoization with dp
+//TABULATION
 class Solution {
 public:
-    vector<vector<int>> dp;
-    bool dfs(int index,int target,vector<int>& nums){
-        if(target==0){
-            return true;
-        }
-        if(index==0){
-            return (nums[0]==target);
-        }
-        if(dp[index][target]!=-1){
-            return dp[index][target];
-        }
-        bool notinc=dfs(index-1,target,nums);
-        bool inc=false;
-        if(nums[index]<=target){
-            inc=dfs(index-1,target-nums[index],nums);
-        }
-        dp[index][target] = notinc || inc ;
-        return dp[index][target];
-    }
+    // vector<vector<int>> dp;
+    // bool dfs(int index,int target,vector<int>& nums){
+    //     if(target==0){
+    //         return true;
+    //     }
+    //     if(index==0){
+    //         return (nums[0]==target);
+    //     }
+    //     if(dp[index][target]!=-1){
+    //         return dp[index][target];
+    //     }
+    //     bool notinc=dfs(index-1,target,nums);
+    //     bool inc=false;
+    //     if(nums[index]<=target){
+    //         inc=dfs(index-1,target-nums[index],nums);
+    //     }
+    //     dp[index][target] = notinc || inc ;
+    //     return dp[index][target];
+    // }
     bool canPartition(vector<int>& nums) {
         int ts=0;
         int n=nums.size();
@@ -30,7 +30,25 @@ public:
             return false;
         }
         int k=ts/2;
-        dp.assign(n,vector<int>(k+1,-1));
-        return dfs(n-1,k,nums);
+        // dp.assign(n,vector<int>(k+1,-1));
+        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+        for(int i=0;i<n;i++){
+            dp[i][0]==true;
+        }
+        if(nums[0]<=k){
+            dp[0][nums[0]]=true;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<=k;j++){
+                bool notinc=dp[i-1][j];
+                bool inc=false;
+                if(nums[i]<=j){
+                    inc=dp[i-1][j-nums[i]];
+                }
+                dp[i][j]=notinc || inc;
+            }
+        }
+        return dp[n-1][k];
+        // return dfs(n-1,k,nums);
     }
 };
