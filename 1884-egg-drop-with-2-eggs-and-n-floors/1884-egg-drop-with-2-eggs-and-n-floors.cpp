0674f4cda96db959d6a3e71a28b93e117c5b891a
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-02-16 08:58:09 +0530

    Time: 7 ms (47.68%), Space: 8.3 MB (17.36%) - LeetHub

---
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int dfs(int k, int n){
+        if(k==0 || n==0){
+            return 0;
+        }
+        if(k==1 || n==1){
+            return n;
+        }
+        if(dp[k][n]!=-1){
+            return dp[k][n];
+        }
+        int ans=1e8;
+        int i=1,j=n;
+        while(i<=j){
+            int m=(i+j)/2;
+            int nb=dfs(k,n-m);
+            int b=dfs(k-1,m-1);
+            ans=min(ans,1+max(nb,b));
+            if(nb>=b){
+                i=m+1;
+            }
+            else{
+                j=m-1;
+            }
+        }
+        return dp[k][n]=ans;
+    }
+    int twoEggDrop(int n) {
+        dp.assign(2+1,vector<int>(n+1,-1));
+        return dfs(2,n);
+    }
+};
