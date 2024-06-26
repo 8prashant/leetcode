045f54205b084a5a91b6ff39fd23147a77f086ff
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-06-26 23:20:16 +0530

    Time: 52 ms (48%), Space: 48.9 MB (10.63%) - LeetHub

--- 2809-create-hello-world-function/2809-create-hello-world-function.js
+++ 2809-create-hello-world-function/2809-create-hello-world-function.js
@@ -1,15 +1,34 @@
-/**
- * @return {Function}
- */
-var createHelloWorld = function() {
+// /**
+//  * @return {Function}
+//  */
+// var createHelloWorld = function() {
     
-    return function(...args) {
-        let ans="Hello World";
-        return ans;
-    }
-};
+//     return function(...args) {
+//         let ans="Hello World";
+//         return ans;
+//     }
+// };
 
-/**
- * const f = createHelloWorld();
- * f(); // "Hello World"
- */
+// /**
+//  * const f = createHelloWorld();
+//  * f(); // "Hello World"
+//  */
+//  function createHelloWorld(){
+//     return function (){
+//         return "Hello World";
+//     };
+// }
+
+
+
+// const f =createHelloWorld();
+// f();
+
+function createHelloWorld(){
+    function f(){
+        return "Hello World";
+    }
+    return f;
+}
+const m=createHelloWorld();
+m();
