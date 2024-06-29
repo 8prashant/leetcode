21420fd0985812b05067b647154208bfe1e71136
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-06-29 21:41:51 +0530

    Time: 351 ms (37.32%), Space: 164.5 MB (11.2%) - LeetHub

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> cp(n);
-        for(int i=0;i<edges.size();i++){
-            cp[edges[i][1]].push_back(edges[i][0]);
+        vector<vector<int>>cc(n);
+        vector<vector<int>>cp(n);
+        for(auto x:edges){
+            cc[x[0]].push_back(x[1]);
+            cp[x[1]].push_back(x[0]);
         }
-        vector<vector<int>> cc(n);
-        for(int i=0;i<edges.size();i++){
-            cc[edges[i][0]].push_back(edges[i][1]);
-        }
-        vector<int> vis(n);
-        queue<int> q;
-        for(int i=0;i<cp.size();i++){
-            vis[i]=cp[i].size();
-            if(vis[i]==0){
+        vector<vector<int>> ans(n);
+        queue<int>q;
+        vector<int>od(n,0);
+        for(int i=0;i<n;i++){
+            od[i]=cp[i].size();
+            if(od[i]==0){
                 q.push(i);
             }
         }
-        vector<vector<int>> ans(n);
-        int i=1;
         while(!q.empty()){
             int a=q.front();
             q.pop();
-            for(auto b:cc[a]){
-                vis[b]-=1;
-                if(vis[b]==0){
-                    q.push(b);
+            for(auto x:cc[a]){
+                od[x]--;
+                if(od[x]==0){
+                    q.push(x);
                 }
             }
             set<int>s;
-            for(auto b:cp[a]){
-                s.insert(b);
-                for(auto c:ans[b]){
-                    s.insert(c);
+            for(auto x:cp[a]){
+                s.insert(x);
+                for(auto y:ans[x]){
+                    s.insert(y);
                 }
             }
-            for(auto b:s){
-                ans[a].push_back(b);
+            vector<int>v;
+            for(auto x:s){
+                v.push_back(x);
             }
+            sort(v.begin(),v.end());
+            ans[a]=v;
         }
         return ans;
+
     }
 };
