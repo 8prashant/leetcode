6a77197777db9b0e6db10da86ce1c47844355985
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-07-06 22:54:48 +0530

    Time: 189 ms (38.12%), Space: 134.8 MB (30.75%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> m;
+        unordered_map<int,int>m;
         m[0]=-1;
         int sum=0;
         for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
-            sum%=k;
-            if (m.find(sum) != m.end()) {
-				if (i - m[sum] > 1) return true;
-			}
-			else m[sum] = i;
+            sum=(sum+nums[i])%k;
+            // cout<<"sum="<<sum<<endl;
+            if(m.find(sum)!=m.end()){
+                // cout<<"sum="<<sum<<" "<<m[sum]<<" "<<i<<endl;
+                if(abs(i-m[sum])>=2){
+                    return true;
+                }
+            }
+            else{
+                m[sum]=i;
+            }
         }
         return false;
-        
     }
 };
