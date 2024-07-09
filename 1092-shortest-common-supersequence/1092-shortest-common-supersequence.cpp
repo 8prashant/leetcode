ebcc387c4e985b8ad958f97a0a06cc4662d9a936
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-07-09 10:26:03 +0530

    Time: 27 ms (30.66%), Space: 30 MB (7.17%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-        int n=s1.size(),m=s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1));
-        for(int i=0;i<=n;i++){
+    string shortestCommonSupersequence(string st1, string st2) {
+        vector<vector<int>>dp(st1.size()+1,vector<int>(st2.size()+1));
+        for(int i=0;i<=st2.size();i++){
+            dp[0][i]=0;
+        }
+        for(int i=0;i<=st1.size();i++){
             dp[i][0]=0;
         }
-        for(int j=0;j<=m;j++){
-            dp[0][j]=0;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
+        for(int i=1;i<=st1.size();i++){
+            for(int j=1;j<=st2.size();j++){
+                if(st1[i-1]==st2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
                 }
                 else{
@@ -19,34 +18,37 @@ public:
                 }
             }
         }
-        int i=n,j=m;
         string ans;
-        while(i!=0 && j!=0){
-            if(s1[i-1]==s2[j-1]){
-                ans=s1[i-1]+ans;
-                i-=1;
-                j-=1;
+        int i=st1.size(),j=st2.size();
+        while(i>=0 && j>=0){
+            if(i==0 && j==0){
+                break;
+            }
+            else if(i==0 && j!=0){
+                ans=st2.substr(0,j)+ans;
+                break;
+            }
+            else if(j==0 && i!=0){
+                ans=st1.substr(0,i)+ans;
+                break;
             }
             else{
-                if(dp[i-1][j]>=dp[i][j-1]){
-                    ans=s1[i-1]+ans;
-                    i-=1;
+                if(st1[i-1]==st2[j-1]){
+                    ans=st1[i-1]+ans;
+                    i--;
+                    j--;
+                }
+                else if(dp[i-1][j]>=dp[i][j-1]){
+                    ans=st1[i-1]+ans;
+                    i--;
                 }
                 else{
-                    ans=s2[j-1]+ans;
-                    j-=1;
+                    ans=st2[j-1]+ans;
+                    j--;
                 }
             }
         }
-        if(i==0 && j==0){
-            return ans;
-        }
-        if(i==0 && j>0){
-            ans=s2.substr(0,j)+ans;
-            return ans;
-        }
-        ans=s1.substr(0,i)+ans;
-        return ans;
 
+        return ans;
     }
 };
