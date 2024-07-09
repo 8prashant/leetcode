5fecf212b6d620f8edb7ecbffa7e156893591b64
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-07-09 20:25:37 +0530

    Time: 15 ms (22.54%), Space: 14 MB (17.67%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,35 +1,52 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int dfs(int i, int j,string &word1, string &word2){
-        if(i<0 && j<0){
-            return 0;
-        }
-        if(i<0 && j>=0){
-            return j+1;
-        }
-        if(j<0 && i>=0){
-            return i+1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
 
-        if(word1[i]==word2[j]){
-            return dp[i][j]=dfs(i-1,j-1,word1,word2);
+//              MEMOIZATION
+
+//     vector<vector<int>>dp;
+//     int check(string &w1, string &w2,int i,int j){
+//         if(i<0 && j<0){
+//             return 0;
+//         }
+//         if(i<0){
+//             return j+1;
+//         }
+//         if(j<0){
+//             return i+1;
+//         }
+//         if (dp[i][j] != -1) {
+//             return dp[i][j];
+//         }
+//         if(w1[i]==w2[j]){
+//             return dp[i][j]=check(w1,w2,i-1,j-1);
+//         }
+//         return dp[i][j]=min({1+check(w1,w2,i-1,j),1+check(w1,w2,i,j-1),1+check(w1,w2,i-1,j-1)});
+//     }
+    int minDistance(string w1, string w2) {
+        // dp.resize(w1.size(),vector<int>(w2.size(),-1));
+        // return check(w1,w2,w1.size()-1,w2.size()-1);
+
+//                               TABULATION
+
+        vector<vector<size_t>> dp(w1.size()+1,vector<size_t>(w2.size()+1));
+        dp[0][0]=0;
+        for(size_t i=1;i<=w2.size();i++){
+            dp[0][i]=i;
         }
-        return dp[i][j]=min({
-        1+dfs(i-1,j,word1,word2),
-        1+dfs(i,j-1,word1,word2),
-        1+dfs(i-1,j-1,word1,word2)});
-    }
-    int minDistance(string word1, string word2) {
-        int n=word1.size(),m=word2.size();
-        if(word2.size()>word1.size()){
-            dp.assign(m,vector<int>(n,-1));
-            return dfs(m-1,n-1,word2,word1);
+        for(size_t i=1;i<=w1.size();i++){
+            dp[i][0]=i;
         }
-        dp.assign(n,vector<int>(m,-1));
-        return dfs(n-1,m-1,word1,word2);
+        for(size_t i=1;i<=w1.size();i++){
+            for(size_t j=1;j<=w2.size();j++){
+                if(w1[i-1]==w2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=min({1+dp[i-1][j],1+dp[i][j-1],1+dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[w1.size()][w2.size()];
+
     }
 };
