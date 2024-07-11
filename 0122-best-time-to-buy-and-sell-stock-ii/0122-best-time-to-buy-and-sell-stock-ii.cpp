174b6f5c79392ced0a6e898c4d7f6027abf5331d
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-07-11 19:58:30 +0530

    Time: 8 ms (36.41%), Space: 18.1 MB (19.32%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,29 +1,49 @@
+                                //MEMOIZATION
+// class Solution {
+// public:
+//     vector<vector<int>> dp;
+//     int check(int i,bool buy,vector<int>& p){
+//         if(i>=p.size()){
+//             return 0;
+//         }
+//         if(dp[i][buy]!=-1){
+//             return dp[i][buy];
+//         }
+//         if(buy){
+//             return dp[i][buy]=max(
+//                 -p[i]+check(i+1,false,p),
+//                 check(i+1,buy,p)
+//             );
+//         }
+//         return dp[i][buy]=max(
+//             +p[i]+check(i+1,true,p),
+//             check(i+1,buy,p)
+//         );
+//     }
+//     int maxProfit(vector<int>& prices) {
+//         dp.resize(prices.size(),vector<int>(2,-1));
+//         return check(0,true,prices);
+//     }
+// };
+                                //TABULATION
+ 
 class Solution {
 public:
-    vector<vector<int>> dp;
-    int dfs(vector<int>& prices,int p,int n , bool buy){
-        if(p==n){
-            return 0;
+    int maxProfit(vector<int>& p) {
+        vector<vector<int>> dp(p.size()+1,vector<int>(2));
+        int n=p.size();
+        dp[n][0]=dp[n][1]=0;
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                if(j==0){
+                    dp[i][j]=max(-p[i]+dp[i+1][1],dp[i+1][0]);
+                }
+                else{
+                    dp[i][j]=max(p[i]+dp[i+1][0],dp[i+1][1]);
+                }
+            }
         }
-        if(dp[p][buy]!=-1){
-            return dp[p][buy];
-        }
-        if(!buy){
-            return dp[p][buy]=max(
-                -(prices[p])+dfs(prices,p+1,n,true),
-                dfs(prices,p+1,n,buy)
-            );
-        }
-        else{
-            return dp[p][buy]=max(
-                prices[p]+dfs(prices,p+1,n,false),
-                dfs(prices,p+1,n,buy)
-            );
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        dp.assign(n,vector<int>(3,-1));
-        return dfs(prices,0,n,false);
+        return dp[0][0];
     }
 };
