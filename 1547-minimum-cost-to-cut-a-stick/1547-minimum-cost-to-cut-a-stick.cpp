a3ca7ff1769a8a1abd73cea694ef26457d8b6b2c
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-07-24 15:39:24 +0530

    Time: 39 ms (38.47%), Space: 12.7 MB (10.73%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    int check(int i ,int j , vector<int>& cuts){
+        if(j-i<=1){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=INT_MAX;
+        for(int l=i+1;l<j;l++){
+            ans=min(ans,(cuts[j]-cuts[i])+check(i,l,cuts)+check(l,j,cuts));
+        }
+        return dp[i][j]=ans;
+    }
+    int minCost(int n, vector<int>& cuts) {
+        dp.resize(cuts.size()+2,vector<int>(cuts.size()+2,-1));
+        sort(cuts.begin(),cuts.end());
+        cuts.insert(cuts.begin(),0);
+        cuts.push_back(n);
+        return check(0,cuts.size()-1,cuts);
+    }
+};
