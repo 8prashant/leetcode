dd81e1e5d91ab3756daa03c153cb66408a556932
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-08-05 02:20:26 +0530

    Time: 197 ms (50.77%), Space: 12.2 MB (36.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     vector<vector<int>>dp;
     int check(int i,int j,vector<int>& nums){
-        if(j-i<=1){
+        if(i>j){
             return 0;
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
         int ans=INT_MIN;
-        for(int l=i+1;l<j;l++){
-            ans=max(ans,(
-                (nums[i]*nums[l]*nums[j])+check(i,l,nums)+check(l,j,nums)
-            ));
+        for(int l=i;l<=j;l++){
+            ans=max(ans,
+                ((nums[i-1]*nums[l]*nums[j+1])+check(i,l-1,nums)+check(l+1,j,nums))
+            );
         }
         return dp[i][j]=ans;
     }
@@ -21,6 +21,6 @@ public:
         nums.push_back(1);
         int n=nums.size();
         dp.resize(n,vector<int>(n,-1));
-        return check(0,n-1,nums);
+        return check(1,nums.size()-2,nums);
     }
 };
