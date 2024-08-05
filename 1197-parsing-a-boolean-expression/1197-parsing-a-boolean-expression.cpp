23285d703aa8c83933c797b9728a5d572be294a9
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-08-05 11:59:53 +0530

    Time: 63 ms (5.27%), Space: 395 MB (5.05%) - LeetHub

--- 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
+++ 1197-parsing-a-boolean-expression/1197-parsing-a-boolean-expression.cpp
@@ -1,50 +1,53 @@
 class Solution {
 public:
-    bool check(int i, int j,string &s){
+    bool check(int i , int j ,string e){
         if(i==j){
-            return s[i]=='t'?true:false;
+            return e[i]=='f'?false:true;
         }
-        int l=i+2,r=i+2;
-        bool ans;
+        if(e[i]=='!'){
+            return check(i+2,j-1,e)==true?false:true;
+        }
+        else if(e[i]=='&'){
+            int count=0;
+            bool ans=true;
+            int l=i+2,r=i+2;
+            while(r<=j){
+                if(e[r]=='('){
+                    count++;
+                }
+                else if(e[r]==')'){
+                    count--;
+                }
+                if(count==0 && (e[r+1]==',' || r+1==j)){
+                    cout<<"next"<<endl;
+                    ans=ans&check(l,r,e);
+                    l=r+2;
+                    r++;
+                }
+                r++;
+            }
+            return ans;
+        }
         int count=0;
-        if(s[i]=='&'){
-            ans=true;
-            while(r<=j){
-                if((s[r]==',' || r==j)&& count==0){
-                    ans&=check(l,r-1,s);
-                    l=r+1;
-                }
-                if(s[r]=='('){
-                    count++;
-                }
-                else if(s[r]==')'){
-                    count--;
-                }
+        bool ans=false;
+        int l=i+2,r=i+2;
+        while(r<=j){
+            if(e[r]=='('){
+                count++;
+            }
+            if(e[r]==')'){
+                count--;
+            }
+            if(count==0 && (e[r+1]==',' || r+1==j)){
+                ans=ans|check(l,r,e);
+                l=r+2;
                 r++;
             }
-        }
-        else if(s[i]=='!'){
-           ans=check(i+2,j-1,s)==true?false:true;
-        }
-        else{
-            ans=false;
-            while(r<=j){
-                if((s[r]==',' || r==j)&& count==0){
-                    ans|=check(l,r-1,s);
-                    l=r+1;
-                }
-                if(s[r]=='('){
-                    count++;
-                }
-                else if(s[r]==')'){
-                    count--;
-                }
-                r++;
-            }
+            r++;
         }
         return ans;
     }
-    bool parseBoolExpr(string ex) {
-        return check(0,ex.size()-1,ex);
+    bool parseBoolExpr(string e) {
+        return check(0,e.size()-1,e);
     }
 };
