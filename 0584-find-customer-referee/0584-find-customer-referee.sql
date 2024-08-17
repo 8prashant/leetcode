de992f36a766c74e19d6e248d4bbd833de950c9c
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-08-17 12:43:28 +0530

    Time: 465 ms (48.19%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,1 +1,4 @@
-SELECT NAME FROM CUSTOMER WHERE referee_id IS NULL OR referee_id!=2;
+# Write your MySQL query statement below
+select name from Customer
+-- where NOT referee_id=2   In SQL, NULL represents an unknown value, and comparisons with NULL do not return TRUE or FALSE; they return NULL (which is treated as FALSE in filtering conditions).
+where referee_id Is null or referee_id!=2
