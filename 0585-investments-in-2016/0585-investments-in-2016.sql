e70bdbbf1749834672026fdf53ab73bf2b43a032
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-08-23 11:09:43 +0530

    Time: 591 ms (19.8%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,9 +1,10 @@
-select sum(tiv_2016) as tiv_2016 from Insurance as it1 join (
-select lat,lon from Insurance
+select round(sum(tiv_2016),2) as tiv_2016 from Insurance as it1 join (
+select it3.tiv_2015,it3.lat,it3.lon from Insurance as it3
+join (select tiv_2015 from Insurance
 group by tiv_2015
-having count(tiv_2015)>1
-union
-select lat,lon from Insurance
+having count(tiv_2015)>1) as it4 on it3.tiv_2015=it4.tiv_2015
+intersect
+select tiv_2015,lat,lon from Insurance
 group by lat,lon
 having count(pid)=1
-) as it2 on it1.lat=it2.lat and it1.lon=it2.lon
+) as it2 on it1.lat=it2.lat and it1.lon=it2.lon and it1.tiv_2015=it2.tiv_2015
