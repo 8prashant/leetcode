acd613efc8e1f73afa7ddf061f4c927ffd934138
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-09-16 12:49:56 +0530

    Time: 6 ms (96.44%), Space: 17.8 MB (31.57%) - LeetHub

---
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findMinDifference(vector<string>& ti) {
+        int n=ti.size();
+        vector<double>t;
+        for(auto x:ti){
+            string h=x.substr(0,2);
+            string m=x.substr(3);
+            double hh=stoi(h);
+            double mm=stoi(m);
+            mm=mm/60;
+            hh+=mm;
+            t.push_back(hh);
+        }
+        sort(t.begin(),t.end());
+        double a=(t[n-1]-t[0])*60;
+        double b=(24-t[n-1]+t[0])*60;
+        int ans = (int)round(min(a, b));
+        for(int i=1;i<n;i++){
+            a=(t[i]-t[i-1])*60;
+            b=(24-t[i]+t[i-1])*60;
+            double mi=min(a,b);
+            ans = min(ans,(int)round(mi));
+        }
+        return ans;
+
+    }
+};
