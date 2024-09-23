c33928bce2501d5087ab1dfc657c7d6d51d016e6
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-09-23 18:34:04 +0530

    Time: 140 ms (49.16%), Space: 75.7 MB (64.87%) - LeetHub

--- 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/2755-extra-characters-in-a-string.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int minExtraChar(string s, vector<string>& dictionary) {
-        int max_val = s.length() + 1;
-        vector<int> dp(s.length() + 1, max_val);
-        dp[0] = 0;
-
-        unordered_set<string> dictionary_set(dictionary.begin(), dictionary.end());
-
-        for (int i = 1; i <= s.length(); ++i) {
-            dp[i] = dp[i - 1] + 1;
-            for (int l = 1; l <= i; ++l) {
-                if (dictionary_set.find(s.substr(i - l, l)) != dictionary_set.end()) {
-                    dp[i] = min(dp[i], dp[i - l]);
-                }
+    set<string>s;
+    vector<int>dp;
+    int check(int i,int n,string &st){
+        if(i==n){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int ans=INT_MAX;
+        string a="";
+        for(int l=i;l<n;l++){
+            a+=st[l];
+            if(s.find(a)!=s.end()){
+                int res=check(l+1,n,st);
+                ans=min(ans,res);
             }
         }
-        return dp.back();
+        int res=1+check(i+1,n,st);
+        ans=min(ans,res);
+        return dp[i]=ans;
+    }
+    int minExtraChar(string st, vector<string>& dictionary) {
+        for(auto x:dictionary){
+            s.insert(x);
+        }
+        dp.resize(st.size(),-1);
+        return check(0,st.size(),st);
+
     }
 };
