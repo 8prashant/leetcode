1dd9e446ef0dbd8eb9d825a64fd23e330e574630
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-09-28 12:22:10 +0530

    Time: 20 ms (64.1%), Space: 22.8 MB (48.19%) - LeetHub

---
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -0,0 +1,69 @@
+class MyCircularDeque {
+public:
+    int n;
+    deque<int>dq;
+    MyCircularDeque(int k) {
+        n=k;
+    }
+    
+    bool insertFront(int value) {
+        if(dq.size()==n){
+            return false;
+        }
+        dq.push_front(value);
+        return true;
+    }
+    
+    bool insertLast(int value) {
+        if(dq.size()==n){
+            return false;
+        }
+        dq.push_back(value);
+        return true;
+    }
+    
+    bool deleteFront() {
+        if(dq.size()==0){
+            return false;
+        }
+        dq.pop_front();
+        return true;
+    }
+    
+    bool deleteLast() {
+        if(dq.size()==0){
+            return false;
+        }
+        dq.pop_back();
+        return true;
+    }
+    
+    int getFront() {
+        return dq.size()==0?-1:dq.front();
+    }
+    
+    int getRear() {
+        return dq.size()==0?-1:dq.back();
+    }
+    
+    bool isEmpty() {
+        return dq.size()==0?true:false;
+    }
+    
+    bool isFull() {
+        return dq.size()==n?true:false;
+    }
+};
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque* obj = new MyCircularDeque(k);
+ * bool param_1 = obj->insertFront(value);
+ * bool param_2 = obj->insertLast(value);
+ * bool param_3 = obj->deleteFront();
+ * bool param_4 = obj->deleteLast();
+ * int param_5 = obj->getFront();
+ * int param_6 = obj->getRear();
+ * bool param_7 = obj->isEmpty();
+ * bool param_8 = obj->isFull();
+ */
