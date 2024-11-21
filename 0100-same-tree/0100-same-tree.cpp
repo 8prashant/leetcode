4a6d5236653ffdd19626b7accc8907cc4c630f8f
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-11-21 12:38:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,13 +11,52 @@
  */
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL){
+    bool isSameTree(TreeNode* p, TreeNode* qt) {
+        if(p==NULL && qt==NULL){
             return true;
         }
-        if(p==NULL && q!=NULL || p!=NULL &&q==NULL || p->val!=q->val){
+        if(p==NULL || qt==NULL){
             return false;
         }
-        return true && isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+        queue<pair<TreeNode*,int>>q;
+        q.push({p,0});
+        map<int,int>m;
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                pair<TreeNode*,int>f=q.front();
+                q.pop();
+                m[f.second]=f.first->val;
+                if(f.first->left!=NULL){
+                    q.push({f.first->left,2*f.second+1});
+                }
+                if(f.first->right!=NULL){
+                    q.push({f.first->right,2*f.second+2});
+                }
+            }
+        }
+        q.push({qt,0});
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                pair<TreeNode*,int>f=q.front();
+                q.pop();
+                if(m.find(f.second)!=m.end()){
+                    if(m[f.second]!=f.first->val){
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+                if(f.first->left!=NULL){
+                    q.push({f.first->left,2*f.second+1});
+                }
+                if(f.first->right!=NULL){
+                    q.push({f.first->right,2*f.second+2});
+                }   
+            }
+        }
+        return true;
     }
 };
