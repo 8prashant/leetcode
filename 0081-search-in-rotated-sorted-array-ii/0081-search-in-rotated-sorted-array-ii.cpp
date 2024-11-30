234d1e6e9b1a096db8fb0cbdec93212873353fc2
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-11-30 21:43:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        return count(nums.begin(),nums.end(),target)==0? false:true;
+        int i=0,j=nums.size()-1;
+        while(i<=j){
+            int mid=(j-i)/2+i;
+            if(nums[mid]==target){
+                return true;
+            }
+            if(nums[i]<=nums[mid]){
+                if(nums[i]<=target && nums[mid]>target){
+                    j=mid-1;
+                }
+                else{
+                    i=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[j]>=target){
+                    i=mid+1;
+                }
+                else{
+                    j=mid-1;
+                }
+            }
+        }
+        return false;
     }
 };
