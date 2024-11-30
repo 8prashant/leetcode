33e789143b5955f10c9de16cad87285792f4a3e6
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2024-11-30 21:59:33 +0530

    Time: 0 ms (100%), Space: 26.1 MB (5.99%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,43 +1,10 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
+        int ans=nums[0];
+        for(int i=1;i<nums.size();i++){
+            ans^=nums[i];
         }
-        int low=0,high=nums.size()-1;
-        if(nums[low]!=nums[low+1]){
-            return nums[low];
-        }
-        if(nums[high]!=nums[high-1]){
-            return nums[high];
-        }
-        low++;
-        high--;
-        while(low<=high){
-            if(low==high){
-                return nums[low];
-            }
-            int mid=low+(high-low)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
-            if(nums[mid-1]==nums[mid]){
-                if(mid%2!=0){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
-            else if(nums[mid]==nums[mid+1]){
-                if(mid%2==0){
-                    low=mid+1;
-                }
-                else{
-                    high=mid-1;
-                }
-            }
-        }
-        return 0;
+        return ans;
     }
 };
