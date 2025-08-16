a48a51f3ef59c3260db800dfc4e316af9624f3a2
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2025-08-17 00:12:38 +0530

    Time: 143 ms (42.6%), Space: 110.6 MB (33.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,54 +1,33 @@
-//TABULATION
 class Solution {
 public:
-    // vector<vector<int>> dp;
-    // bool dfs(int index,int target,vector<int>& nums){
-    //     if(target==0){
-    //         return true;
-    //     }
-    //     if(index==0){
-    //         return (nums[0]==target);
-    //     }
-    //     if(dp[index][target]!=-1){
-    //         return dp[index][target];
-    //     }
-    //     bool notinc=dfs(index-1,target,nums);
-    //     bool inc=false;
-    //     if(nums[index]<=target){
-    //         inc=dfs(index-1,target-nums[index],nums);
-    //     }
-    //     dp[index][target] = notinc || inc ;
-    //     return dp[index][target];
-    // }
-    bool canPartition(vector<int>& nums) {
-        int ts=0;
-        int n=nums.size();
-        for(int i=0;i<nums.size();i++){
-            ts+=nums[i];
+    vector<vector<int>>dp;
+    bool res(int x,int t,vector<int>&v){
+        if(t==0){
+            return true;
         }
-        if(ts%2!=0){
+        if(x<0){
             return false;
         }
-        int k=ts/2;
-        // dp.assign(n,vector<int>(k+1,-1));
-        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
-        for(int i=0;i<n;i++){
-            dp[i][0]==true;
+        if(dp[x][t]!=-1){
+            return dp[x][t];
         }
-        if(nums[0]<=k){
-            dp[0][nums[0]]=true;
+        bool notinc=res(x-1,t,v);
+        bool inc=false;
+        if(t>=v[x]){
+            inc=res(x-1,t-v[x],v);
         }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=k;j++){
-                bool notinc=dp[i-1][j];
-                bool inc=false;
-                if(nums[i]<=j){
-                    inc=dp[i-1][j-nums[i]];
-                }
-                dp[i][j]=notinc || inc;
-            }
+        return dp[x][t]=notinc || inc?1:0;
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(auto x:nums){
+            sum+=x;
         }
-        return dp[n-1][k];
-        // return dfs(n-1,k,nums);
+        if(sum%2!=0){
+            return false;
+        }
+        int target=sum/2;
+        dp.resize(nums.size(),vector<int>(target+1,-1));
+        return res(nums.size()-1,target,nums);
     }
 };
