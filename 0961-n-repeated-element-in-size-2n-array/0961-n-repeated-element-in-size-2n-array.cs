3d0f90c1147f56a1c8eb0e438a622167c5136a31
Prashant Kumar Rai <prashant.kumar2021@vitbhopal.ac.in>
2026-01-02 14:44:10 +0530

    Time: 0 ms (100%), Space: 50.7 MB (47.54%) - LeetHub

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public int RepeatedNTimes(int[] nums) {
+        for(int i=0;i<nums.Length-2;i++){
+            if(nums[i]==nums[i+1] || nums[i]==nums[i+2]){
+                return nums[i];
+            }
+        }
+        return nums[nums.Length-1];
+    }
+}
